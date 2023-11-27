53449693fa196fbac41feff64409545a369fa519
Fanur GB-2525 <makhiyanov.ff@gmail.com>
2023-11-27 22:32:15 +0300

    created employeeList

---
+++ src/main/java/ru/geekbrains/StreamAPI/Employee.java
@@ -0,0 +1,59 @@
+package ru.geekbrains.StreamAPI;
+
+public class Employee {
+    public String name;
+    public int age;
+    public double salary;
+    public String department;
+
+    public Employee(String name, int age, double salary, String department) {
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+        this.department = department;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    @Override
+    public String toString() {
+//        return "Employee{" +
+//                "name='" + name + '\'' +
+//                ", age=" + age +
+//                ", salary=" + salary +
+//                ", department='" + department + '\'' +
+//                '}';
+        return String.format("[%s] Age: %s y.o.; Salary: %s $; Department: %s", name, age, salary, department);
+    }
+}
+
---
+++ src/main/java/ru/geekbrains/StreamAPI/Main.java
@@ -0,0 +1,39 @@
+package ru.geekbrains.StreamAPI;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/*
+2. Создать класс Employee (Сотрудник) с полями: String name, int age, double salary, String department
+   * 2.1 Создать список из 10-20 сотрудников
+   * 2.2 Вывести список всех различных отделов (department) по списку сотрудников
+   * 2.3 Всем сотрудникам, чья зарплата меньше 10_000, повысить зарплату на 20%
+   * 2.4 * Из списка сотрудников с помощью стрима создать Map<String, List<Employee>> с отделами и сотрудниками внутри отдела
+   * 2.5 * Из списока сорудников с помощью стрима создать Map<String, Double> с отделами и средней зарплатой внутри отдела
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Employee> employeeList = List.of(
+                new Employee("Sergei", 42, 2500.00, "Sales"),
+                new Employee("Maxim", 38, 2500.00, "Sales"),
+                new Employee("Ilya", 40, 2700.00, "Technical"),
+                new Employee("Fanur", 37, 3500.00, "Technical"),
+                new Employee("Petr", 55, 11900.00, "Logistics"),
+                new Employee("Elena", 40, 2850.00, "Sales"),
+                new Employee("Alexander", 29, 2100.00, "Technical"),
+                new Employee("Kseniya", 25, 2200.00, "Sales"),
+                new Employee("Boris", 27, 2500.00, "Logistics"),
+                new Employee("Nikita", 23, 12000.00, "Sales"),
+                new Employee("Vasiliy", 33, 3500.00, "Technical"),
+                new Employee("Roman", 26, 4000.00, "Sales"),
+                new Employee("Pavel", 25, 15000.00, "Technical"),
+                new Employee("Anastasia", 34, 15500.00, "Logistics")
+        );
+
+        System.out.println(employeeList);
+
+    }
+}
